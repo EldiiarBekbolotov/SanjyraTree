ed28bd32c8d5a7834d4eb849a3552a9ec17ecbb1
ebekalternate <ebekalternate@gmail.com>
2024-11-30 22:49:06 +0000

    renamed:    public/assets/.idx/dev.nix -> public/.idx/dev.nix 	modified:   public/css/main.css 	modified:   public/editor.html 	modified:   public/index.html 	modified:   public/js/index.js

--- public/js/index.js
+++ public/js/index.js
@@ -5,7 +5,14 @@ function $(selector) {
         return document.querySelectorAll(selector);
     }
 }
+function checkClassSupport() {
+    const testElement = document.createElement('div');
+    testElement.classList.add('test-class');
+    const hasClassList = testElement.classList.contains('test-class');
+    testElement.classList.remove('test-class');
 
+    return hasClassList;
+}
 function scene(index) {
     window.scroll({
         top: 0,
@@ -17,12 +24,28 @@ function scene(index) {
     }
     $('.scene')[index].style.display = 'block';
 
+    for (var i = 0; i < $('.nav-link').length; i++) {
+
+        if (checkClassSupport()) {
+
+            document.getElementsByClassName('nav-link')[i].classList.remove('active');
+        } else {
+            $('.nav-link')[i].style.borderBottom = '0px solid white';
+        }
+
+    }
+    if (checkClassSupport()) { 
+        document.getElementsByClassName('nav-link')[index].classList.add('active'); 
+    } else {
+        $('.nav-link')[index].style.color = 'white';
+        $('.nav-link')[index].style.borderBottom = '2px solid white';
+    }
+
     if (window.innerWidth < 992) { // Check if viewport width is <992px (Bootstrap's md breakpoint)
         let navbar = document.getElementById('navbarSupportedContent');
         let bsCollapse = new bootstrap.Collapse(navbar);
-        bsCollapse.hide(); 
+        bsCollapse.hide();
     }
 }
 
 scene(0);
-
